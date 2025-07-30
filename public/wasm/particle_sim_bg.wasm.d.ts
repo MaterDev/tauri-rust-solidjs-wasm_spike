d6489d3fe6eda877e3036ab06a3594d666774026
Key Clark <aryeh.the.lion@gmail.com>
2025-07-30 00:47:49 -0500

    feat: add WebAssembly particle simulation with TypeScript bindings

---
+++ public/wasm/particle_sim_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_simulation_free: (a: number, b: number) => void;
+export const simulation_new: (a: number) => number;
+export const simulation_tick: (a: number) => [number, number];
+export const simulation_get_positions: (a: number) => [number, number];
+export const simulation_get_count: (a: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
